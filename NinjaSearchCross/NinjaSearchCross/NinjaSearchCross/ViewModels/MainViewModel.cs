e03b872383f6d9a73b7a9a7f27ed04df34d351ab
Stefan Nilsson <stfnilsson@hotmail.se>
2015-12-19 14:22:00 +0100

    Base architecture for Ninja Search Cross

---
+++ NinjaSearchCross/NinjaSearchCross/NinjaSearchCross/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NinjaSearchCross.Models;
+using ReactiveUI;
+using NinjaSearchCross.Localization;
+
+namespace NinjaSearchCross.ViewModels
+{
+    public class MainViewModel : ReactiveObject
+    {
+        public string Title => Main.Title;
+
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
